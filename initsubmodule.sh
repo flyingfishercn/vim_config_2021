a592b0a624633a0a61669f43b1b0ad0349f86b3e
Harris Huang <4525613+flyingfishercn@users.noreply.github.com>
2021-10-22 10:02:52 +0800

    Create initsubmodule.sh
    vim插件脚本列表

---
+++ initsubmodule.sh
@@ -0,0 +1,78 @@
+[submodule "bundle/pathogen.vim"]
+	path = bundle/pathogen.vim
+	url = https://github.com/vim-scripts/pathogen.vim.git
+[submodule "bundle/a.vim"]
+	path = bundle/a.vim
+	url = https://github.com/vim-scripts/a.vim.git
+[submodule "bundle/vim-indent-guides"]
+	path = bundle/vim-indent-guides
+	url = https://github.com/nathanaelkane/vim-indent-guides.git
+[submodule "bundle/vimspell"]
+	path = bundle/vimspell
+	url = https://github.com/vim-scripts/vimspell.git
+[submodule "bundle/bandit.vim"]
+	path = bundle/bandit.vim
+	url = https://github.com/vim-scripts/bandit.vim.git
+[submodule "bundle/bufexplorer.zip"]
+	path = bundle/bufexplorer.zip
+	url = https://github.com/vim-scripts/bufexplorer.zip.git
+[submodule "bundle/CRefVim"]
+	path = bundle/CRefVim
+	url = https://github.com/vim-scripts/CRefVim.git
+[submodule "bundle/cscope.vim"]
+	path = bundle/cscope.vim
+	url = https://github.com/vim-scripts/cscope.vim.git
+[submodule "bundle/c.vim"]
+	path = bundle/c.vim
+	url = https://github.com/vim-scripts/c.vim.git
+[submodule "bundle/genutils"]
+	path = bundle/genutils
+	url = https://github.com/vim-scripts/genutils.git
+[submodule "bundle/lookupfile"]
+	path = bundle/lookupfile
+	url = https://github.com/vim-scripts/lookupfile.git
+[submodule "bundle/OmniCppComplete"]
+	path = bundle/OmniCppComplete
+	url = https://github.com/vim-scripts/OmniCppComplete.git
+[submodule "bundle/nerdtree"]
+	path = bundle/nerdtree
+	url = https://github.com/preservim/nerdtree.git
+[submodule "bundle/python.vim"]
+	path = bundle/python.vim
+	url = https://github.com/vim-scripts/python.vim.git
+[submodule "bundle/rainbow_parentheses.vim"]
+	path = bundle/rainbow_parentheses.vim
+	url = https://github.com/vim-scripts/rainbow_parentheses.vim.git
+[submodule "bundle/repeat.vim"]
+	path = bundle/repeat.vim
+	url = https://github.com/vim-scripts/repeat.vim.git
+[submodule "bundle/surround.vim"]
+	path = bundle/surround.vim
+	url = https://github.com/vim-scripts/surround.vim.git
+[submodule "bundle/Tabular"]
+	path = bundle/Tabular
+	url = https://github.com/vim-scripts/Tabular.git
+[submodule "bundle/Tagbar"]
+	path = bundle/Tagbar
+	url = https://github.com/vim-scripts/Tagbar.git
+[submodule "bundle/YankRing.vim"]
+	path = bundle/YankRing.vim
+	url = https://github.com/vim-scripts/YankRing.vim.git
+[submodule "bundle/taglist.vim"]
+	path = bundle/taglist.vim
+	url = https://github.com/vim-scripts/taglist.vim.git
+[submodule "bundle/ctags.vim"]
+	path = bundle/ctags.vim
+	url = https://github.com/vim-scripts/ctags.vim.git
+[submodule "bundle/nerdcommenter"]
+	path = bundle/nerdcommenter
+	url = https://github.com/preservim/nerdcommenter.git
+[submodule "bundle/SuperTab"]
+	path = bundle/SuperTab
+	url = https://github.com/vim-scripts/SuperTab.git
+[submodule "bundle/buffergrep"]
+	path = bundle/buffergrep
+	url = https://github.com/vim-scripts/buffergrep.git
+[submodule "bundle/winmanager"]
+	path = bundle/winmanager
+	url = https://github.com/vim-scripts/winmanager.git
